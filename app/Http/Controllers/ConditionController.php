6e684af1cc6600fdf9acf30a5f377263701f5bdc
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-03-18 23:39:02 -0500

    feat: add ConditionController

---
+++ app/Http/Controllers/ConditionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreconditionRequest;
+use App\Http\Requests\UpdateconditionRequest;
+use App\Models\Condition;
+
+class ConditionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreconditionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Condition $condition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Condition $condition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateconditionRequest $request, Condition $condition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Condition $condition)
+    {
+        //
+    }
+}
