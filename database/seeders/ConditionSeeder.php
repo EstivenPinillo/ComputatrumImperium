a05bfdd8f0bdb7fc83bb5485f05478f30e00e97d
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-03-18 23:17:25 -0500

    feat: add seeders and factories of the condition class

---
+++ database/seeders/ConditionSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Condition;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConditionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Condition::factory()->count(1)->create();
+    }
+}
