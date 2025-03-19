9e49b72fa2a942324f2c3e7b72bd57d4ead226a0
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-03-18 21:09:06 -0500

    feat: add conditions table the migrations

---
+++ database/migrations/2025_03_19_001415_create_conditions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conditions', function (Blueprint $table) {
+            $table->id();
+            $table->string('condition', length: 100);
+            $table->string('description', length: 150);
+            $table->timestamps();
+            $table->softDeletes('deleted_at', precision: 0);
+            $table->index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conditions');
+    }
+};
