1086e8c32142f55cbeba627935e402fa08131212
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-03-21 23:20:43 -0500

    feat: add profiles migration

---
+++ database/migrations/2025_03_22_032004_create_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('condition_id');
+            $table->foreign('condition_id')->references('id')->on('conditions');
+            $table->string('profile', length: 100);
+            $table->string('description', length: 100);
+            $table->string('permits', length: 600);
+            $table->timestamps();
+            $table->softDeletes('deleted_at', precision: 0);
+            $table->index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
