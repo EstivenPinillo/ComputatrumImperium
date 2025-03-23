804fcc746bc19a6a1af4caac3361b09a22656239
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-03-22 23:37:30 -0500

    add factory of type document. change databaseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ConditionSeeder::class,
             ProfileSeeder::class,
-            CompanySeeder::class
+            CompanySeeder::class,
+            TypeDocumentSeeder::class
         ]);
     }
 }
