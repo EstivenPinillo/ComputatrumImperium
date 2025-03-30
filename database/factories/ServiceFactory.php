64057aab423c6785fbb1d54f056b7afac9879973
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-03-29 22:45:51 -0500

    feat: add service factory

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Service>
+ */
+class ServiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'company_id' => 1,
+            'user_id' => 1,
+            'computer_id' => 1,
+            'condition_id' => 1,
+        ];
+    }
+}
