4bcef45829cd619f78af60f190f9283c409e9482
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-03-30 23:54:44 -0500

    feat: add user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'profile_id' => 1,
+            'type_document_id' => 1,
+            'condition_id' => 1,
+            'document_number' => fake()->randomNumber(),
             'name' => fake()->name(),
+            'last_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
