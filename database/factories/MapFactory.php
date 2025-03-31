898b4033195fca0ba8a39d38c789f96d51ada21a
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-03-30 23:49:10 -0500

    feat: add map factory

---
+++ database/factories/MapFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Map>
+ */
+class MapFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 1,
+            'condition_id' => 1,
+            'name' => fake()->word(),
+            'description' => fake()->text(90),
+        ];
+    }
+}
