21955692a1d99ad43a675206c645e4973a0b80bb
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-05 22:02:58 -0500

    feat: add location to databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
             AccessorySeeder::class,
             MapSeeder::class,
             AreaSeeder::class,
+            LocationSeeder::class,
         ]);
     }
 }
