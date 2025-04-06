a0f925ffc1d815a6022eac348d3a2a910d7bbd3c
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-06 17:33:36 -0500

    feat: add user_computer_locations controller

---
+++ app/Http/Controllers/UserComputerLocationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserComputerLocationRequest;
+use App\Http\Requests\UpdateUserComputerLocationRequest;
+use App\Models\UserComputerLocation;
+
+class UserComputerLocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserComputerLocationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserComputerLocation $userComputerLocation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserComputerLocation $userComputerLocation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserComputerLocationRequest $request, UserComputerLocation $userComputerLocation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserComputerLocation $userComputerLocation)
+    {
+        //
+    }
+}
