aa15c3143c115b279b2b66cff1085b3e32044023
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-06 21:37:19 -0500

    feat: add re-entry controller

---
+++ app/Http/Controllers/ReEntryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreReEntryRequest;
+use App\Http\Requests\UpdateReEntryRequest;
+use App\Models\ReEntry;
+
+class ReEntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReEntryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReEntry $reEntry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ReEntry $reEntry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReEntryRequest $request, ReEntry $reEntry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReEntry $reEntry)
+    {
+        //
+    }
+}
