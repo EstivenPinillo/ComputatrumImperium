079a96f25aeb730270a0f4d1706e23757e7604fc
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-07 19:01:57 -0500

    feat: add TransferAccessoryReEntry model

---
+++ app/Models/TransferAccessoryReEntry.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransferAccessoryReEntry extends Model
+{
+    /** @use HasFactory<\Database\Factories\TransferAccessoryReEntryFactory> */
+    use HasFactory;
+}
