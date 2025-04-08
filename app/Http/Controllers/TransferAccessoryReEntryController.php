1993315dc2298cf31be20aa5f9d1b3e0e5f7ecdd
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-07 19:03:10 -0500

    feat: add TransferAccessoryReEntry controller

---
+++ app/Http/Controllers/TransferAccessoryReEntryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTransferAccessoryReEntryRequest;
+use App\Http\Requests\UpdateTransferAccessoryReEntryRequest;
+use App\Models\TransferAccessoryReEntry;
+
+class TransferAccessoryReEntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTransferAccessoryReEntryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TransferAccessoryReEntry $transferAccessoryReEntry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TransferAccessoryReEntry $transferAccessoryReEntry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTransferAccessoryReEntryRequest $request, TransferAccessoryReEntry $transferAccessoryReEntry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TransferAccessoryReEntry $transferAccessoryReEntry)
+    {
+        //
+    }
+}
