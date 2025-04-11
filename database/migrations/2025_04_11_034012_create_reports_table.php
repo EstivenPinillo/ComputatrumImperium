a50f193ca231976829b194c31760a36dbef62163
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-10 23:01:01 -0500

    feat: add report migration

---
+++ database/migrations/2025_04_11_034012_create_reports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('report', length: 150);
+            $table->string('description', length: 300);
+            $table->timestamps();
+            $table->softDeletes('deleted_at', precision: 0);
+            $table->index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
